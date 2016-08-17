2783c0f895ea77d9fe4453f103d1e33d848eff44
Cloud User <centos@ip-172-31-10-210.ap-northeast-1.compute.internal>
2016-08-17 11:03:24 +0000

    mod

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 exports.serializer = require('./lib/zengin_serializer.js')
 exports.parser = require('./lib/zengin_parser.js')
+exports.read = require('./lib/zengin_fileread.js')
+exports.write = require('./lib/zengin_filewrite.js')
