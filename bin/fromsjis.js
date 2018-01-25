e1f8ae09c2942e2fe60cfb3b7f6b88dff5b39d72
you21979 <you2197901@gmail.com>
2018-01-25 19:53:16 +0900

    mod

--- bin/fromsjis.js
+++ bin/fromsjis.js
@@ -2,6 +2,6 @@ const zengin = require("..")
 const fs = require('fs')
 
 const res = zengin.util.fromjis(fs.readFileSync(process.argv.slice(2)[0]))
-console.log(res)
+fs.writeFileSync("./utf8file", res, "utf8")
 
 
--- bin/tosjis.js
+++ bin/tosjis.js
@@ -1,7 +1,11 @@
 const zengin = require("..")
 const fs = require('fs')
 
-const res = zengin.util.tojis(fs.readFileSync(process.argv.slice(2)[0]))
+const crlf = (x) => {
+    return x.split("\r\n").join('').split("\n").join("")
+}
+
+const res = zengin.util.tojis(crlf(fs.readFileSync(process.argv.slice(2)[0], "utf8")))
 fs.writeFileSync("./sjisfile", res, "binary")
 
 
