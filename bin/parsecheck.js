646cb5aa717c6b9771e6d018db048d918f0c4072
you21979 <you2197901@gmail.com>
2018-01-25 19:59:51 +0900

    mod

--- bin/parsecheck.js
+++ bin/parsecheck.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 
 try{
     const res = zengin.read.readFile(process.argv.slice(2)[0])
+    fs.writeFileSync("./decode.txt", JSON.stringify(res, null, 2), "utf8")
 }catch(e){
     console.log(e.message)
     console.log(e.debug.map( v => zengin.util.fromjis(v) ))
